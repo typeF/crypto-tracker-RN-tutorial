15a4a34d31e969ac8520069aca6eb101529de663
Frank Lam <frank.is.missing@gmail.com>
2018-03-11 16:24:59 -0700

    Added basic header with styling

--- App.js
+++ App.js
@@ -5,21 +5,19 @@ import Header from './src/components';
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
+      <View> 
         <Header/>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+ container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
